5cc6e8ea9fb4b1b3fe96c6e78b327280b287a6ce
hubi <hubert1234.94@gmail.com>
2022-07-06 23:28:32 +0200

    Test and fix template in header component

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { By } from '@angular/platform-browser';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,16 +10,103 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      imports: [MatIconModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create header component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should trigger onResize function on window resize', () => {
+    const component = fixture.componentInstance;
+    const spyOnResize = spyOn(component, 'onResize');
+    window.dispatchEvent(new Event('resize'));
+    expect(spyOnResize).toHaveBeenCalled();
+  });
+
+  it('should automatically close the menu after resizing to certain width (see header component for value)', () => {
+    const component = fixture.componentInstance;
+    component.isMenuOpen = true;
+    expect(component.isMenuOpen).toBe(true);
+
+    window.resizeTo(800, 800);
+    window.dispatchEvent(new Event('resize'));
+    expect(component.isMenuOpen).toBe(false);
+  });
+
+  it('should close the mobile menu on smaller screens after clicking any link (see header component for value)', () => {
+    const component = fixture.componentInstance;
+    component.isMenuOpen = false;
+    component.width = 500;
+    component.height = 500;
+    expect(component.isMenuOpen).toBe(false);
+
+    component.menuToggleHandler();
+    expect(component.isMenuOpen).toBe(true);
+  });
+
+  it('should not pop mobile menu on wider screens after clicking any link (see header component for value, should be impossible to do)', () => {
+    const component = fixture.componentInstance;
+    component.isMenuOpen = true;
+    component.width = 900;
+    component.height = 900;
+    expect(component.isMenuOpen).toBe(true);
+
+    component.menuToggleHandler();
+    expect(component.isMenuOpen).toBe(false);
+    component.menuToggleHandler();
+    expect(component.isMenuOpen).toBe(false);
+  });
+
+  it('should return true when menu is open and it is small width screen', () => {
+    const component = fixture.componentInstance;
+    component.isMenuOpen = true;
+    component.width = 500;
+    component.height = 500;
+    expect(component.isMenuOpenAndSmallScreen()).toBe(true);
+  });
+
+  it('should return false when menu is not open and it is small width screen', () => {
+    const component = fixture.componentInstance;
+    component.isMenuOpen = false;
+    component.width = 500;
+    component.height = 500;
+    expect(component.isMenuOpenAndSmallScreen()).toBe(false);
+  });
+
+  it('should return false no matter if menu is open or not for large width screen', () => {
+    const component = fixture.componentInstance;
+    component.isMenuOpen = true;
+    component.width = 800;
+    component.height = 800;
+    expect(component.isMenuOpenAndSmallScreen()).toBe(false);
+    component.isMenuOpen = false;
+    expect(component.isMenuOpenAndSmallScreen()).toBe(false);
+  });
+
+  it('should render burger menu (small screen)', () => {
+    const component = fixture.nativeElement;
+    component.isMenuOpen = false;
+    component.width = 500;
+    component.height = 500;
+    fixture.detectChanges();
+    expect(component.querySelector(".header__content--toggle__burger")).not.toBeNull();
+    expect(component.querySelector(".header__content--toggle__close")).toBeNull();
+  });
+
+  it('should render close icon when menu is open (small screen)', () => {
+    const component = fixture.componentInstance;
+    component.isMenuOpen = true;
+    component.width = 500;
+    component.height = 500;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('.header__content--toggle__burger'))).toBeNull();
+    expect(fixture.debugElement.query(By.css('.header__content--toggle__close'))).not.toBeNull();
   });
 });
