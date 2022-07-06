86db07c7264d03311c0f579b7a754e3763addbc7
hubi <hubert1234.94@gmail.com>
2022-07-06 23:27:21 +0200

    Test layout component

--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 import { LayoutComponent } from './layout.component';
 
@@ -8,16 +10,30 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
-    })
-    .compileComponents();
+      declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create layout component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render header, custom content and footer', () => {
+    const fixture = TestBed.createComponent(LayoutComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const content = document.createElement('div');
+    content.className = 'test';
+    content.innerHTML = 'test';
+    compiled.appendChild(content);
+
+    expect(compiled.querySelector('app-header')).toBeTruthy();
+    expect(compiled.querySelector('app-footer')).toBeTruthy();
+    expect(compiled.querySelector('.test')?.className).toContain('test');
   });
 });
