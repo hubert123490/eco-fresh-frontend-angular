7813a81b306a8c96892b0c721296715a595067f2
hubi <hubert1234.94@gmail.com>
2022-07-07 22:30:25 +0200

    Test landing-page component

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CompanyValuesComponent } from './company-values/company-values.component';
+import { CustomersStoriesComponent } from './customers-stories/customers-stories.component';
 
 import { LandingPageComponent } from './landing-page.component';
+import { QueriesComponent } from './queries/queries.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,16 +12,32 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        LandingPageComponent,
+        WelcomeComponent,
+        CompanyValuesComponent,
+        CustomersStoriesComponent,
+        QueriesComponent,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create landing page component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render welcome, company values, customer stories and app queries in no particular order', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('app-welcome')).toBeTruthy();
+    expect(compiled.querySelector('app-company-values')).toBeTruthy();
+    expect(compiled.querySelector('app-customers-stories')).toBeTruthy();
+    expect(compiled.querySelector('app-queries')).toBeTruthy();
   });
 });
