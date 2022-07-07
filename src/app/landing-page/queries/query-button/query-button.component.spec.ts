acc5c804983538eebc01048b41ba8262e4960e0d
hubi <hubert1234.94@gmail.com>
2022-07-07 22:33:12 +0200

    Test and fix template in query-button component

--- src/app/landing-page/queries/query-button/query-button.component.spec.ts
+++ src/app/landing-page/queries/query-button/query-button.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { QueryButtonComponent } from './query-button.component';
 
@@ -8,16 +9,29 @@ describe('QueryButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QueryButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [QueryButtonComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(QueryButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create query button component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display custom text', () => {
+    component.text = 'test';
+    const text = fixture.nativeElement.querySelector('.text');
+    fixture.detectChanges();
+    expect(text.textContent).toEqual('test');
+  });
+
+  it('should display custom content', () => {
+    component.text = 'test';
+    const text = fixture.nativeElement.querySelector('.text');
+    fixture.detectChanges();
+    expect(text.textContent).toEqual('test');
   });
 });
