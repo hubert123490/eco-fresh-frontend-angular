a0451e3b458e18001c9137a57499d4e885b38081
hubi <hubert1234.94@gmail.com>
2022-07-28 22:32:25 +0200

    Add component for handling product form

---
+++ src/app/product-details/product-details-order/product-details-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-order',
+  templateUrl: './product-details-order.component.html',
+  styleUrls: ['./product-details-order.component.scss']
+})
+export class ProductDetailsOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
