eb425ca9f8230d9827d2d0a4e72c97738057958f
hubi <hubert1234.94@gmail.com>
2022-08-01 21:54:25 +0200

    Fix product-details test

--- src/app/product-details/product-details.component.spec.ts
+++ src/app/product-details/product-details.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 import { ProductDetailsComponent } from './product-details.component';
 
@@ -8,9 +11,9 @@ describe('ProductDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductDetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductDetailsComponent],
+      imports: [RouterTestingModule.withRoutes([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
