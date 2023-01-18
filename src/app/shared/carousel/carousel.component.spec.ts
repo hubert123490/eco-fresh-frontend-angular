22da63b9e154a803dad8919eda08c984701c7966
hubi <hubert1234.94@gmail.com>
2023-01-18 21:08:28 +0100

    WIP carousel implementation

--- src/app/shared/carousel/carousel.component.spec.ts
+++ src/app/shared/carousel/carousel.component.spec.ts
@@ -8,16 +8,24 @@ describe('CarouselComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CarouselComponent ]
-    })
-    .compileComponents();
+      declarations: [CarouselComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', async () => {
     expect(component).toBeTruthy();
   });
+
+  it('getCurrentSlideUrl works', async () => {
+    const expected = "url('./../../../assets/landing-page/welcome.jpg')"
+    component.slides = [
+      { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+    ];
+    fixture.detectChanges();
+    expect(component.getCurrentSlideUrl()).toEqual(`url('${component.slides[0].url}')`);
+  })
 });
