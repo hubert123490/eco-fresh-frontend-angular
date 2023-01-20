48543399dad492d0f61f062d8b540c9b32dbe7ac
hubi <hubert1234.94@gmail.com>
2023-01-20 23:04:48 +0100

    Add carousel

--- src/app/shared/carousel/carousel.component.spec.ts
+++ src/app/shared/carousel/carousel.component.spec.ts
@@ -13,6 +13,10 @@ describe('CarouselComponent', () => {
 
     fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
+    component.slides = component.slides = [
+      { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+      { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+    ];
     fixture.detectChanges();
   });
 
@@ -20,12 +24,60 @@ describe('CarouselComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('getCurrentSlideUrl works', async () => {
-    const expected = "url('./../../../assets/landing-page/welcome.jpg')"
+  it('getCurrentSlideUrl() works', async () => {
     component.slides = [
       { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
     ];
     fixture.detectChanges();
-    expect(component.getCurrentSlideUrl()).toEqual(`url('${component.slides[0].url}')`);
-  })
+    expect(component.getCurrentSlideUrl()).toEqual(
+      `url('${component.slides[0].url}')`
+    );
+  });
+
+  it('goToNext() works', () => {
+    [
+      { current: 1, expectedResult: 0 },
+      { current: 0, expectedResult: 1 },
+    ].forEach(({ current, expectedResult }) => {
+      component.slides = [
+        { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+        { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+      ];
+      component.currentIndex = current;
+      fixture.detectChanges();
+      component.goToNext();
+
+      expect(component.currentIndex).toEqual(expectedResult);
+    });
+  });
+
+  it('goToPrevious() works', () => {
+    [
+      { current: 0, expectedResult: 1 },
+      { current: 1, expectedResult: 0 },
+    ].forEach(({ current, expectedResult }) => {
+      component.slides = [
+        { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+        { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+      ];
+      component.currentIndex = current;
+      fixture.detectChanges();
+      component.goToPrevious();
+
+      expect(component.currentIndex).toEqual(expectedResult);
+    });
+  });
+
+  it('goToSlide(index) works', () => {
+    const expectedResult = 1;
+    component.slides = [
+      { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+      { url: './../../../assets/landing-page/welcome.jpg', title: 'tmp' },
+    ];
+    component.currentIndex = 0;
+    fixture.detectChanges();
+    component.goToSlide(1);
+
+    expect(component.currentIndex).toEqual(expectedResult);
+  });
 });
