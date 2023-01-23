e08d373b18a78a6f7eee0f74648ebb523b327602
hubi <hubert1234.94@gmail.com>
2023-01-23 21:39:37 +0100

    Add oAuth component

---
+++ src/app/auth/o-auth/o-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-o-auth',
+  templateUrl: './o-auth.component.html',
+  styleUrls: ['./o-auth.component.scss']
+})
+export class OAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
