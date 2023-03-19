77b7e25112a5440a10f291bae89a55e18808780c
hubi <hubert1234.94@gmail.com>
2023-03-19 20:18:43 +0100

    Change cart service

--- src/app/services/cart.service.spec.ts
+++ src/app/services/cart.service.spec.ts
@@ -3,20 +3,26 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Cart } from '../store/models/Cart';
+import { cartState } from '../store/reducers/cart.reducer';
 
 import { CartService } from './cart.service';
 
 describe('CartService', () => {
   let sut: CartService;
   let httpMock: HttpTestingController;
+  let store: MockStore;
+  let initialState = { cart: { ...cartState } };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [CartService],
+      providers: [CartService, provideMockStore({ initialState })],
     });
     sut = TestBed.inject(CartService);
     httpMock = TestBed.inject(HttpTestingController);
+    store = TestBed.inject(MockStore);
   });
 
   afterEach(() => {
@@ -26,4 +32,46 @@ describe('CartService', () => {
   it('creates CartService', () => {
     expect(sut).toBeTruthy();
   });
+
+  it('makes a GET request to retrieve cart items', async () => {
+    // given
+    const expectedResult: Cart = mockedCart;
+
+    // when
+    sut.getCartData().subscribe((result) => {
+      // then
+      expect(result).toEqual(expectedResult);
+    });
+
+     // then
+     const req = httpMock.expectOne(
+      '/assets/data/cart.json'
+    );
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockedCart);
+  });
 });
+
+const mockedCart: Cart = {
+  cartItems: [
+    {
+      productId: "0",
+      orderRequest: {
+        kcalChoice: '2000',
+        mealsAmountChoice: 3,
+      },
+      productPrice: 32.23,
+      productImage: {
+        src: 'assets/main-page/products/chicken__soup.jpg',
+        alt: 'chicken soup',
+      },
+    },
+  ],
+  cartSummary: {
+    totalPrice: 20.99,
+    subTotal: 5,
+    shipping: 5,
+    tax: 10,
+  },
+};
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,10 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Cart } from '../store/models/Cart';
+import { selectCartItems } from '../store/selectors/cart.selectors';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  constructor(private http: HttpClient, private store: Store) {}
 
-  constructor(private http: HttpClient) { }
+  public getCartData(): Observable<Cart> {
+    return this.http.get<Cart>('/assets/data/cart.json', {
+      params: {
+        cartItems: this.getCartItems(),
+      },
+    });
+  }
+
+  private getCartItems() {
+    let cartItems: any;
+    this.store.select(selectCartItems).subscribe((items) => {
+      cartItems = items;
+    });
+    return cartItems;
+  }
 }
