b3295979df9001cbea5ce89a50a5623249ea4e28
hubi <hubert1234.94@gmail.com>
2023-03-19 20:18:27 +0100

    Add loadCart logic and effect

--- src/app/store/actions/cart.actions.ts
+++ src/app/store/actions/cart.actions.ts
@@ -1,5 +1,5 @@
 import { createActionGroup, props } from '@ngrx/store';
-import { CartItemAddRequest, CartItemQuantityChangeRequest, CartItemRemoveRequest } from '../models/Cart';
+import { Cart, CartItemAddRequest, CartItemQuantityChangeRequest, CartItemRemoveRequest } from '../models/Cart';
 
 export const CartApiActions = createActionGroup({
   source: 'Order API',
@@ -7,6 +7,7 @@ export const CartApiActions = createActionGroup({
     'Add Item': props<{request : CartItemAddRequest}>(),
     'Remove Item' : props<{request : CartItemRemoveRequest}>(),
     'Add Item Quantity' : props<{request : CartItemQuantityChangeRequest}>(),
-    'Reduce Item Quantity' : props<{request : CartItemQuantityChangeRequest}>()
+    'Reduce Item Quantity' : props<{request : CartItemQuantityChangeRequest}>(),
+    'Load Cart' : props<{cart : Cart}>(),
   },
 });
--- src/app/store/effects/cart.effects.ts
+++ src/app/store/effects/cart.effects.ts
@@ -4,12 +4,21 @@ import { of } from 'rxjs';
 import { map, mergeMap, catchError } from 'rxjs/operators';
 import { CartService } from 'src/app/services/cart.service';
 import { CartApiActions } from '../actions/cart.actions';
+import { Cart } from '../models/Cart';
 
 @Injectable()
 export class CartEffects {
+  loadCart$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CartApiActions.addItem, CartApiActions.removeItem, CartApiActions.addItemQuantity, CartApiActions.reduceItemQuantity),
+      mergeMap(() =>
+        this.cartService.getCartData().pipe(
+          map((cart: Cart) => CartApiActions.loadCart({ cart })),
+          catchError(() => of())
+        )
+      )
+    )
+  );
 
-  constructor(
-    private actions$: Actions,
-    private cartService: CartService
-  ) {}
+  constructor(private actions$: Actions, private cartService: CartService) {}
 }
