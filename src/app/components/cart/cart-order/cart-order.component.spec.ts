ad0464386b839d1059ee5c4f1f837fa3d443e0a6
hubi <hubert1234.94@gmail.com>
2023-03-22 20:34:03 +0100

    Update cart order component

--- src/app/components/cart/cart-order/cart-order.component.spec.ts
+++ src/app/components/cart/cart-order/cart-order.component.spec.ts
@@ -3,21 +3,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CartOrderComponent } from './cart-order.component';
 
 describe('CartOrderComponent', () => {
-  let component: CartOrderComponent;
+  let sut: CartOrderComponent;
   let fixture: ComponentFixture<CartOrderComponent>;
+  const cartSummary = {
+    totalPrice: 20.99,
+    subTotal: 5,
+    shipping: 5,
+    tax: 10,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartOrderComponent ]
-    })
-    .compileComponents();
+      declarations: [CartOrderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CartOrderComponent);
-    component = fixture.componentInstance;
+    sut = fixture.componentInstance;
+    sut.cartSummary = cartSummary;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('creates CartOrder', () => {
+    expect(sut).toBeTruthy();
   });
 });
