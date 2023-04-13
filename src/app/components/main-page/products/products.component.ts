0de11c439be6a84221d5447b186e2e05fd696c4b
hubi <hubert1234.94@gmail.com>
2023-04-13 18:31:16 +0200

    Add http resource handling to products

--- src/app/components/main-page/products/products.component.ts
+++ src/app/components/main-page/products/products.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Product, ProductsSize } from 'src/app/store/models/Product';
 import { Store } from '@ngrx/store';
-import { selectProducts, selectProductsSize } from 'src/app/store/selectors/products.selectors';
+import {
+  selectProducts,
+  selectProductsSize,
+} from 'src/app/store/selectors/products.selectors';
 import { Observable } from 'rxjs';
 import { PaginationApiActions } from 'src/app/store/actions/pagination.actions';
+import { HttpResourceHandlerService } from '../../shared/http-resource/http-resource-handler/http-resource-handler.service';
 
 @Component({
   selector: 'app-products',
@@ -11,13 +15,16 @@ import { PaginationApiActions } from 'src/app/store/actions/pagination.actions';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-  products$ : Observable<readonly Product[]> = this.store.select(selectProducts);
-  productsSize$ : Observable<ProductsSize> = this.store.select(selectProductsSize);
+  products$: Observable<readonly Product[]> = this.store.select(selectProducts);
+  productsSize$: Observable<ProductsSize> =
+    this.store.select(selectProductsSize);
 
-  constructor(private store : Store) {
-  }
+  constructor(
+    private store: Store,
+    public httpResourceService: HttpResourceHandlerService
+  ) {}
 
   ngOnInit(): void {
-      this.store.dispatch(PaginationApiActions.loadProductsSize())
+    this.store.dispatch(PaginationApiActions.loadProductsSize());
   }
 }
