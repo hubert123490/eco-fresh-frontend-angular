08d765222dd890d53fa1aab2845a6fa7660b56ca
hubi <hubert1234.94@gmail.com>
2023-05-15 17:36:30 +0200

    Change products selector

--- src/app/store/selectors/products.selectors.ts
+++ src/app/store/selectors/products.selectors.ts
@@ -1,5 +1,12 @@
-import { createFeatureSelector } from '@ngrx/store';
-import { Product, ProductsSize } from '../models/Product';
- 
-export const selectProducts = createFeatureSelector<ReadonlyArray<Product>>('products');
-export const selectProductsSize = createFeatureSelector<Readonly<ProductsSize>>('productsSize');
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { Product } from '../models/Product';
+
+export const selectProducts =
+  createFeatureSelector<ReadonlyArray<Product>>('products');
+
+export const selectProductsPage = (page: number) =>
+  createSelector(selectProducts, (state: ReadonlyArray<Product>) => {
+    return state.slice((page - 1) * 6, page * 6);
+  });
+
+export const selectProductsLength = createSelector(selectProducts, (state: ReadonlyArray<Product>) => state.length)
