9d9e0697d0569011be759989902f9648e02bc249
hubi <hubert1234.94@gmail.com>
2023-07-09 21:26:52 +0200

    Add filters to products service

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,18 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Product } from '../store/models/Product';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FilterRequest } from '../store/models/Filter';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http : HttpClient
-  ) { }
+  getProducts(filters?: FilterRequest): Observable<Product[]> {
+    if (filters) {
+      const params = this.getFiltersHttpParams(filters);
+      return this.http.get<Product[]>('http://localhost:8080/products', {
+        params: params,
+      });
+    }
+    return this.http.get<Product[]>('http://localhost:8080/products');
+  }
 
-  getProducts() : Observable<Array<Product>> {
-    return this.http.get<Product[]>('/assets/data/products.json');
+  private getFiltersHttpParams(filters: FilterRequest) {
+    let params = new HttpParams();
+
+    if(filters.productNamePrefix) params = params.set('productNamePrefix', filters.productNamePrefix);
+    if(filters.priceFrom) params = params.set('priceFrom', filters.priceFrom);
+    if(filters.priceTo) params = params.set('priceTo', filters.priceTo);
+    if(filters.categories) filters.categories.forEach((category) => {
+      params = params.append('categories', category);
+    });
+    if(filters.certifications) filters.certifications.forEach((certification) => {
+      params = params.append('certifications', certification);
+    });
+
+    return params;
   }
 }
