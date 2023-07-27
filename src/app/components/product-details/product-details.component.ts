4f6ea24ab99f054522adf86e78e6d48cc6200537
hubi <hubert1234.94@gmail.com>
2023-07-27 20:49:40 +0200

    Update product details component

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { ProductDetailsApiActions } from 'src/app/store/actions/product-details.actions';
 import { ProductDetails } from 'src/app/store/models/ProductDetails';
 import { selectProductDetails } from 'src/app/store/selectors/product-details.selectors';
+import { HttpResourceHandlerService } from '../shared/http-resource/http-resource-handler/http-resource-handler.service';
 
 @Component({
   selector: 'app-product-details',
@@ -17,11 +18,11 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private store: Store
+    private store: Store,
+    public httpResourceService: HttpResourceHandlerService
   ) {}
 
   ngOnInit(): void {
-    // first get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = String(routeParams.get('productId'));
     this.store.dispatch(
