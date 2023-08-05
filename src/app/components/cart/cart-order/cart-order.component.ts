f79bb8905da03644cdbb5b05307db10ef8d42706
hubi <hubert1234.94@gmail.com>
2023-08-05 22:10:35 +0200

    Update cart order component

--- src/app/components/cart/cart-order/cart-order.component.ts
+++ src/app/components/cart/cart-order/cart-order.component.ts
@@ -1,16 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
-import { CartSummary } from 'src/app/store/models/Cart';
+import { loadStripe } from '@stripe/stripe-js';
+import { CartItem } from 'src/app/store/models/Cart';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cart-order',
   templateUrl: './cart-order.component.html',
   styleUrls: ['./cart-order.component.scss']
 })
-export class CartOrderComponent implements OnInit {
-  @Input() cartSummary : CartSummary | undefined;
+export class CartOrderComponent {
+  @Input() cartItems : CartItem[] = [];
+  // We load  Stripe
+  stripePromise = loadStripe(environment.stripe);
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  async pay(): Promise<void> {
+    // here we create a payment object
+    const payment = {
+      checkoutProducts : this.cartItems,
+      cancelUrl: 'https://localhost:4200/cancel',
+      successUrl: 'https://localhost:4200/success',
+    };
 
-  ngOnInit(): void {
+    const stripe = await this.stripePromise;
+
+    // this is a normal http calls for a backend api
+    this.http
+      .post(`${environment.serverUrl}/payment`, payment)
+      .subscribe((data: any) => {
+        // I use stripe to redirect To Checkout page of Stripe platform
+        stripe!.redirectToCheckout({
+          sessionId: data.id,
+        });
+      });
   }
 }
