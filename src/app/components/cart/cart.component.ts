6e793ee04b99ca6d52239cc6cd6d395c7c0cc7ba
hubi <hubert1234.94@gmail.com>
2023-08-05 22:10:25 +0200

    Update cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { Cart } from 'src/app/store/models/Cart';
-import { selectCart } from 'src/app/store/selectors/cart.selectors';
+import {
+  selectCart,
+  selectCartItems,
+} from 'src/app/store/selectors/cart.selectors';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  cart$: Observable<Cart> =
-    this.store.select(selectCart);
+  cartItems$ = this.store.select(selectCartItems);
 
-  constructor(private store : Store) { }
+  constructor(private store: Store) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
