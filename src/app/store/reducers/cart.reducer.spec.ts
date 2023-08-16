101719a7eab81b4258c0b54b025839f56a37c085
hubi <hubert1234.94@gmail.com>
2023-08-16 19:49:10 +0200

    Update cart reducer test

--- src/app/store/reducers/cart.reducer.spec.ts
+++ src/app/store/reducers/cart.reducer.spec.ts
@@ -9,38 +9,47 @@ describe('Cart Reducer', () => {
     cartState = [
         {
           id: '1',
-          quantity: 1
+          quantity: 1,
+          name : 'test name 1',
+          imageUrl: 'https://domain.image1.com'
         },
         {
           id: '2',
-          quantity: 2
+          quantity: 2,
+          name : 'test name 2',
+          imageUrl: 'https://domain.image2.com'
         }
       ]
   });
   
   it('adds item to cart when item is new', () => {
-    const action = CartApiActions.addItem({ productId: '4', quantity: 4 });
+    const action = CartApiActions.addItem({ productId: '4', quantity: 4, name: 'test name 4', imageUrl: 'https://domain.image4.com' });
     const state = cartReducer([...cartState], action);
 
-    const expectedResult = [...cartState, {id : '4', quantity: 4}];
+    const expectedResult = [...cartState, { id: '4', quantity: 4, name: 'test name 4', imageUrl: 'https://domain.image4.com' }];
 
     expect(state).toEqual(expectedResult);
   });
 
   it('adds quantity to an item when item in cart exists', () => {
     const action = CartApiActions.addItem({
-      productId: '1', quantity: 4
+      productId: '1', quantity: 4, name : 'test name 1',
+      imageUrl: 'https://domain.image1.com'
     });
     const state = cartReducer([ ...cartState ], action);
 
     const expectedResult = [
       {
         id: '1',
-        quantity: 5
+        quantity: 5,
+        name : 'test name 1',
+        imageUrl: 'https://domain.image1.com'
       },
       {
         id: '2',
-        quantity: 2
+        quantity: 2,
+        name : 'test name 2',
+          imageUrl: 'https://domain.image2.com'
       }
     ];
 
