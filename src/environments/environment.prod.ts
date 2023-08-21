0624f1ded769b6faf88ee590f6d488b2613504e0
hubi <hubert1234.94@gmail.com>
2023-08-22 01:30:20 +0200

    Update envrionment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   stripe: 'pk_test_51MT3XtKm5P8pm2jnfU7CIJravWrynFcVTVJcILUTnyHe3i7MQtLTrMemNVLKKiLYybxLsuSiVWsrK0XDHCG742vQ007RFtF0Re',
-  serverUrl: 'https://eco-fresh-service-3f6acf681853.herokuapp.com/',
+  serverUrl: 'https://eco-fresh-service-3f6acf681853.herokuapp.com',
   cancelUrl: 'https://hubert123490.github.io/eco-fresh-frontend-angular/',
   okUrl: 'https://hubert123490.github.io/eco-fresh-frontend-angular/'
 };
