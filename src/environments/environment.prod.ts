62110fd7395aca96347742de9cb11eb12c845a59
hubi <hubert1234.94@gmail.com>
2023-08-22 01:12:33 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  stripe: 'pk_test_51MT3XtKm5P8pm2jnfU7CIJravWrynFcVTVJcILUTnyHe3i7MQtLTrMemNVLKKiLYybxLsuSiVWsrK0XDHCG742vQ007RFtF0Re',
+  serverUrl: 'https://eco-fresh-service-3f6acf681853.herokuapp.com/'
 };
