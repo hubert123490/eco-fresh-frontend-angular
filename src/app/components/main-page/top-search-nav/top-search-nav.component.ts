2a2ed016396f9dc1574bccfcb177bcb3177499ff
hubi <hubert1234.94@gmail.com>
2023-08-25 21:01:47 +0200

    Update top search nav component

--- src/app/components/main-page/top-search-nav/top-search-nav.component.ts
+++ src/app/components/main-page/top-search-nav/top-search-nav.component.ts
@@ -8,18 +8,6 @@ import { FilterApiActions } from 'src/app/store/actions/filter.actions';
   selector: 'app-top-search-nav',
   templateUrl: './top-search-nav.component.html',
   styleUrls: ['./top-search-nav.component.scss'],
-  animations: [
-    trigger('inOutAnimation', [
-      transition(':enter', [
-        style({ height: 0, opacity: 0 }),
-        animate('0.5s ease-out', style({ height: '*', opacity: 1 })),
-      ]),
-      transition(':leave', [
-        style({ height: '*', opacity: 1 }),
-        animate('0.5s ease-in', style({ height: 0, opacity: 0 })),
-      ]),
-    ]),
-  ],
 })
 export class TopSearchNavComponent {
   @Input() responsiveFilter?: ResponsiveFilter;
